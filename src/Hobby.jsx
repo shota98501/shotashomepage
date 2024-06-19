c165c3dcc3a7f7497c8bae319d8c33398891a9de
shota98501 <114859169+shota98501@users.noreply.github.com>
2024-06-19 21:12:51 +0900

    push

---
+++ src/Hobby.jsx
@@ -0,0 +1,31 @@
+import React,{useState} from "react";
+import FadeInSection from "./FadeinSection";
+import './Hobby.css'
+
+function Hobby (){
+    const [setState] = useState({
+        expanded: true,
+        activeKey: "1"
+    });
+
+    const handleChange = (eventKey) =>{
+        setState({
+            activeKey: eventKey
+        })
+    }
+
+    const one = (
+        <p>
+            Outside of work and studying, I like to engage to hobbies
+        </p>
+    );
+
+    const desc_item = [one]
+
+    const hobby_stack = [
+        "Gaming",
+        "Reading",
+        "Training",
+        "Travel",
+    ]
+}
